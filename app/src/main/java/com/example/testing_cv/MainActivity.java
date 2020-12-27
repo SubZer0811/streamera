f2997c2a288e0071377b26ff9ed4ec881cec2277
subzer0 <subzer0@ultron.sm>
2020-12-27 15:44:22 +0530

    First Commit | STABLE

---
+++ app/src/main/java/com/example/testing_cv/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.testing_cv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.Image;
+import android.os.Bundle;
+
+import org.opencv.core.Mat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
